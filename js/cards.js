96305560a53c19304163da4a9f42bb9e18b610d7
Seven091 <seven.91@hotmail.fr>
2026-07-31 23:50:48 +0200

    Update cards.js

--- js/cards.js
+++ js/cards.js
@@ -1,8 +1,36 @@
 /*
 =========================================
 L'ÉCHO DU DÉSIR
-Gestion des cartes
+Chargement des cartes
 =========================================
 */
 
-export const Challenges = [];
+let challenges = [];
+
+export async function loadChallenges() {
+
+    const response = await fetch("./data/challenges.json");
+
+    challenges = await response.json();
+
+    return challenges;
+
+}
+
+export function getChallenges() {
+
+    return challenges;
+
+}
+
+export function getChallenge(id) {
+
+    return challenges.find(card => card.id === id);
+
+}
+
+export function getPhase(phase) {
+
+    return challenges.filter(card => card.phase === phase);
+
+}
